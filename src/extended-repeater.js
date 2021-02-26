d97581d246b84ea1e99a4ff02e9766253029ca5e
maksguido1987 <maksguido@gmail.com>
2021-02-26 20:41:46 +0300

    feat: extented-repeats

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let res = ''
+  let addRes = ''
+  
+  let repeatTimes = options.repeatTimes || 1
+  let separator = options.separator || "+"
+  let addition = options.addition || ''
+  let additionSeparator = options.additionSeparator || '|'
+  let additionRepeatTimes = options.additionRepeatTimes || 1
+
+  if (typeof str !== 'string') {
+    str = String(str)
+  }
+  if (options.addition === false) {
+    addition = "false"
+  }
+  if (options.addition === null) {
+    addition = "null"
+  }
+
+  if (additionRepeatTimes !== undefined) {
+    for (let j = 0; j <= additionRepeatTimes - 1; j++) {
+      if (j === additionRepeatTimes - 1) additionSeparator = ''
+      
+      addRes += `${addition}${additionSeparator}`
+    }
+  }
+
+  if (repeatTimes !== undefined) {
+    for (let i = 0; i <= repeatTimes - 1; i++) {
+      if (i === repeatTimes - 1) separator = ''
+  
+      res += `${str}${addRes}${separator}`
+      
+    }
+  }
+
+  return res;
+
 };
   
