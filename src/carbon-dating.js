78d41bcbebf7577b6c732c33ef679efb42a5f3c7
maksguido1987 <maksguido@gmail.com>
2021-02-27 23:30:43 +0300

    fit: add new tests and fix carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,9 +5,8 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (typeof sampleActivity !== 'string') return false;
-  if (Number(sampleActivity) !== sampleActivity) return false
-  if (sampleActivity == null) return false
-  if (+sampleActivity > 15) return false
+  if (Number.isNaN(+sampleActivity)) return false
+  if (+sampleActivity > MODERN_ACTIVITY) return false
   if (+sampleActivity <= 0 ) return false
 
   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (Math.log(2) / HALF_LIFE_PERIOD))
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -11,4 +11,5 @@ class VigenereCipheringMachine {
   }
 }
 
+
 module.exports = VigenereCipheringMachine;
